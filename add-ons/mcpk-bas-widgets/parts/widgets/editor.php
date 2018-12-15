321e0b0b27f484e2e6144024ed2b60cbab38203c
Donna McMaster <donna@donnamcmaster.com>
2018-12-14 22:02:52 -0800

    edit indentation to match file in another site

--- add-ons/mcpk-bas-widgets/parts/widgets/editor.php
+++ add-ons/mcpk-bas-widgets/parts/widgets/editor.php
@@ -7,14 +7,13 @@ Width: 600
 
 use McPik\Basics\Utils;
 
-	echo $before_widget;
-	if ( Utils\first_leaf( $settings['show_title'] ) && !empty( $settings['title'] ) ) {
-		echo $before_title;
-		echo wptexturize( $settings['title'] );
-		echo $after_title;
-	}
-	if ( !empty( $settings['content'] ) ) {
-		echo wptexturize( wpautop( $settings['content'] ) );
-	}
-	echo $after_widget;
-?>
+echo $before_widget;
+if ( Utils\first_leaf( $settings['show_title'] ) && !empty( $settings['title'] ) ) {
+	echo $before_title;
+	echo wptexturize( $settings['title'] );
+	echo $after_title;
+}
+if ( !empty( $settings['content'] ) ) {
+	echo wptexturize( wpautop( $settings['content'] ) );
+}
+echo $after_widget;
