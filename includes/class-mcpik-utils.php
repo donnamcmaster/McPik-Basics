d6de72a7ff7fd437534e9725110d518c21da783f
Donna McMaster <donna@donnamcmaster.com>
2018-12-14 22:12:12 -0800

    add several functions to Utils class; tweak a couple of existing ones
    functions added: 
    - get_request_var
    - get_list_item
    - get_dl_entry
    - display_error_result
    - get_post_anchor (with title, permalink)
    - format_content (apply content filters outside the loop)
    - format_pub_date, normalize_pub_date (yyyy-mm-dd)
    
    functions renamed: 
    - mcw_get_top_cat_slug => get_top_cat_slug
    - mcw_is_family => is_family
    - mcw_is_bloggish => is_bloggish
    
    other:
    - added optional $title to function get_anchor

--- includes/class-mcpik-utils.php
+++ includes/class-mcpik-utils.php
@@ -39,6 +39,22 @@ public static function get_array_element ( $array, $key, $null_value=null ) {
 	return array_key_exists( $key, $array ) ? $array[$key] : $null_value;
 }
 
+/**
+ *	Function get_request_var
+ *	- pluck an element from $_GET, $_POST, or $_REQUEST array
+ *	- does basic sanitization
+ *	- test against null if empty string is a valid option
+ */
+public static function get_request_var ( $var_name, $var_set ) {
+	if ( array_key_exists( $var_name, $var_set ) ) {
+		return trim( $var_set[$var_name] );
+	} else {
+		return null;
+	}
+}
+
+public static function array_to_list () {
+}
 
 /********************
  *
@@ -50,9 +66,9 @@ public static function get_parm ( $name, $value ) {
 	return $value ? ( ' ' . $name . '="' . $value . '"' ) : '';
 }
 
-public static function get_anchor ( $url, $text, $class='' ) {
+public static function get_anchor ( $url, $text, $class='', $title='' ) {
 	if ( $url ) {
-		return '<a href="' . $url .'"'. self::get_parm( 'class', $class ) . '>' . $text . '</a>';
+		return '<a href="' . $url .'"'. self::get_parm( 'class', $class ) . self::get_parm( 'title', $title ) . '>' . $text . '</a>';
 	} else {
 		return null;
 	}
@@ -66,6 +82,22 @@ public static function get_anchor_blank ( $url, $text, $class='' ) {
 	}
 }
 
+public static function get_list_item ( $item, $class='' ) {
+	$class = $class ? " class=\"$class\"" : '';
+	return "<li$class>$item</li>";
+}
+
+public static function get_dl_entry ( $dterm, $ddefinition ) {
+	if ( !trim( $ddefinition ) ) {
+		return '';
+	} else {
+		return "
+		<dt>$dterm:</dt>
+		<dd>$ddefinition</dd>
+		";
+	}
+}
+
 
 /********************
  *
@@ -73,8 +105,27 @@ public static function get_anchor_blank ( $url, $text, $class='' ) {
  *
  ********************/
 
+public static function display_error_result ( $result ) {
+	$error_string = $result->get_error_message();
+?>
+	<div id="message" class="error"><p><?= $error_string;?></p></div>
+<?php
+}
+
+
 /**
- *	Function mcw_is_bloggish
+ *	Function get_post_anchor
+ *	- returns an <a> element linking to the post permalink
+ */
+public static function get_post_anchor ( $id ) {
+	$p = get_post( $id );
+	$url = get_permalink( $id );
+	$name = wptexturize( $p->post_title );
+	return mcw_get_anchor( $url, $name );
+}
+
+/**
+ *	Function is_bloggish
  *	- checks to see if this page is some sort of blog page
  */
 public static function is_bloggish () {
@@ -87,7 +138,7 @@ public static function is_bloggish () {
 }
 
 /**
- *	Function mcw_is_family
+ *	Function is_family
  *	- checks to see if this page is $ancestor or a descendant of $ancestor
  *	- $ancestor must be page ID, not name
  */
@@ -106,7 +157,7 @@ public static function is_family ( $ancestor ) {
 }
 
 /**
- *	Function mcw_get_top_cat_slug
+ *	Function get_top_cat_slug
  *	- in a hierarchical taxonomy, returns the slug for the highest cat in this post's taxonomy
  */
 public static function get_top_cat_slug ( $taxonomy, $post_id=null ) {
@@ -153,5 +204,67 @@ public static function get_custom_value ( $field, $custom_fields ) {
 		return $custom_fields[$field][0];
 	}
 }
+
+/**
+ *	Format content outside the loop
+ *	- receives raw content (from post_content or meta table)
+ *  - returns formatted content
+ *	- applies filters from the_content() 
+ *	- if $has_shortcodes, also expands them
+ */
+public static function format_content ( $content, $has_shortcodes ) {
+	if ( !$content ) {
+		return '';
+
+	} elseif ( $has_shortcodes ) {
+		$extra_junk = array(
+			'<p>['    => '[',
+			']</p>'   => ']',
+			']<br />' => ']'
+		);
+		return do_shortcode( strtr( wpautop( wptexturize( $content ) ), $extra_junk ) );
+
+	} else {
+		return wpautop( wptexturize( $content ) );
+	}
+}
+
+
+/********************
+ *
+ *	Data Formatting Utilities
+ *
+ ********************/
+
+/**
+ *	Format Publication Date
+ *	- publication date format is yyyy-mm-dd for sort purposes
+ *	- but sometimes dd=00 because there is no day, e.g., May 2008
+ *	- PHP date functions barf on yyyy-mm-00 or yyyy-mm 
+ *	- so this function works around it
+ */
+public static function format_pub_date ( $date, $month_only='F Y', $month_day='M j, Y' ) {
+	if ( substr( $date, -2, 2 ) == '00' ) {
+		$date = substr_replace( $date, '01', -2, 2 );
+		$date_format = $month_only;
+	} else {
+		$date_format = $month_day;
+	}
+	return date( $date_format, strtotime( $date ) );
+}
+
+// converts a string '2018-02-30' or '2002-00-00' to something we can insert into WordPress
+public static function normalize_pub_date ( $pub_date ) {
+	$date_parts = explode( '-', $pub_date );
+	if ( $date_parts[1] == '00' ) {
+		$date_parts[1] = '01';
+	}
+	if ( $date_parts[2] == '00' ) {
+		$date_parts[2] = '01';
+	}
+	$clean_date = implode( '-', $date_parts );
+	return $clean_date . ' 00:00:00';
+}
+
 
 } // class McPik_Utils
